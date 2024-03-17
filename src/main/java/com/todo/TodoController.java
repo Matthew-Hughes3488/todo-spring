5bc2cae1f6f15399f9b263bc865a8eb180391248
Matthew <12hughesm@gmail.com>
2024-03-17 17:37:36 +0000

    Finished hooking up the controller with spring

--- src/main/java/com/todo/TodoController.java
+++ src/main/java/com/todo/TodoController.java
@@ -1,9 +1,25 @@
 package com.todo;
 
+import com.todo.task.TaskManager;
+import com.todo.utils.InputManager;
+import com.todo.utils.OutputManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TodoController {
+    private TaskManager taskManager;
+    private OutputManager outputManager;
+    private InputManager inputManager;
+
+    @Autowired
+    public TodoController(TaskManager taskManager, @Qualifier("outputManager1") OutputManager outputManager, InputManager inputManager) {
+        this.taskManager = taskManager;
+        this.outputManager = outputManager;
+        this.inputManager = inputManager;
+    }
+
     public void run() {
     }
 }
--- src/main/java/com/todo/config/AppConfig.java
+++ src/main/java/com/todo/config/AppConfig.java
@@ -1,10 +1,25 @@
 package com.todo.config;
 
+import com.todo.utils.OutputManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.todo")
 public class AppConfig {
+
+    @Bean
+    public OutputManager outputManager1() {
+        String[] inputArray1 = {"New Task", "Set Task As Completed", "Delete Task", "List Tasks"};
+        return new OutputManager(inputArray1);
+    }
+
+    @Bean
+    public OutputManager outputManager2() {
+        String[] inputArray2 = {"value4", "value5", "value6"};
+        return new OutputManager(inputArray2);
+    }
 
 }
